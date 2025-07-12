a4fb28db645f21056a449b199e6b309a07884b1c
Chetan <cckamble2004@gmail.com>
2025-07-12 13:20:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function openCertificate(src, title, issued, org) {
+    
     document.getElementById("certificateModal").style.display = "block";
+
+    
     document.getElementById("modalImage").src = src;
-    document.getElementById("modalInfo").innerHTML = `
-      <h3>${title}</h3>
-      <p>Issued: ${issued}</p>
-      <p>Organization: ${org}</p>
-    `;
-  }
-  
-  function closeModal() {
+
+   
+    document.getElementById("modalInfo").innerHTML = 
+        <h3>${title}</h3>
+        <p>Issued: ${issued}</p>
+        <p>Organization: ${org}</p>
+    ;
+}
+
+
+function closeModal() {
     document.getElementById("certificateModal").style.display = "none";
-  }
-  
+}
+
+function resume() {
+    document.getElementById("resumeImage").style.display = "block";
+}
